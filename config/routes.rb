32dfe2d6e5411dea66c5d172ea8b655034372b54
Mark J Smith <mark@markjs.net>
2013-02-04 23:10:41 +0000

    Nicer routes for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 OfficeDrinkRounds::Application.routes.draw do
-  resources :sessions
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :users
 
