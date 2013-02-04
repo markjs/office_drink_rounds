78ca1e7820c85fb08542f8c1763c9a3410cbf35c
Mark J Smith <mark@markjs.net>
2013-02-04 22:25:46 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OfficeDrinkRounds::Application.config.secret_token = 'cbb3910a6e41a178ebbd4aa971e2174deff22ecf45b74811feb16391c3f58f83d66663c2453c5fd21fb7b5fc246ac90b98559dd02f3441fea94b3d6fc2bd2bd4'
