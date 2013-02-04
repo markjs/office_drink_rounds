72b95faa58b160883b9ca13924dc059eb8753433
Mark J Smith <mark@markjs.net>
2013-02-04 23:16:54 +0000

    Fix broken routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ OfficeDrinkRounds::Application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
+  resources :sessions
 
   resources :users
 
