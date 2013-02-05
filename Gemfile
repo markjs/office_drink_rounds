1025d10617fcd1dee5d410989aa008181bd7d55c
Mark J Smith <mark@markjs.net>
2013-02-05 13:28:08 +0000

    Tests with factory girl and stuff

--- Gemfile
+++ Gemfile
@@ -30,4 +30,5 @@ end
 
 group :test do
   gem "shoulda-matchers"
+  gem 'factory_girl_rails'
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+
+  it { should respond_to :email }
+  it { should validate_presence_of :email }
+  it { should validate_presence_of :password_digest }
+
+  describe "email address" do
+    subject { user }
+
+    it "should allow valid email address" do
+      build(:user).should be_valid
+    end
+
+    it "should not allow invalid email address" do
+      build(:user, email: "invalid").should_not be_valid
+    end
+  end
 end
