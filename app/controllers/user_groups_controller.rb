67ae316e24ba570de71eaacf4856ef9da3feaccb
Mark J Smith <mark@markjs.net>
2013-02-06 13:34:03 +0000

    Basic user group controller stuff

--- app/controllers/user_groups_controller.rb
+++ app/controllers/user_groups_controller.rb
@@ -1,2 +1,29 @@
 class UserGroupsController < ApplicationController
+
+  def show
+  end
+
+  def new
+    @user_group = UserGroup.new
+  end
+
+  def create
+    @user_group = User.new params[:user_group]
+    if @user_group.save
+      redirect_to @user_group, notice: 'User group created'
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user_group.update_attributes params[:user_group]
+      redirect_to @user_group, notice: 'User group updated'
+    else
+      render 'edit'
+    end
+  end
 end
