96767c0fb751c0ea39fd250bd425798b2e334b8a
Mark J Smith <mark@markjs.net>
2013-02-06 13:20:26 +0000

    Rails g resource UserGroup name

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 OfficeDrinkRounds::Application.routes.draw do
+  resources :user_groups
+
+
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
