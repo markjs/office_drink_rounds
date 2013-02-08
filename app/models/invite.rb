28b6d139ee878103838d4f046436bcb735fa2389
Mark J Smith <mark@markjs.net>
2013-02-08 13:56:56 +0000

    Validate presence and email-ness of email

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,6 +1,8 @@
 class Invite < ActiveRecord::Base
   attr_accessible :email
 
+  validates :email, presence: true, email: true
+
   belongs_to :inviter, class_name: 'user'
   belongs_to :user_group
 end
