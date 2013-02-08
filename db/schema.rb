23e367417eff2d67e599e8f7643bb6e57e1a270b
Mark J Smith <mark@markjs.net>
2013-02-08 13:50:07 +0000

    Invite migration generated stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130206132200) do
+ActiveRecord::Schema.define(:version => 20130208134413) do
+
+  create_table "invites", :force => true do |t|
+    t.integer  "inviter_id"
+    t.integer  "user_group_id"
+    t.string   "email"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
 
   create_table "user_groups", :force => true do |t|
     t.string   "name"
