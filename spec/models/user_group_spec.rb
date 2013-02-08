e83e01f5d931626b6f4c42ba441e1c146d96f6c8
Mark J Smith <mark@markjs.net>
2013-02-08 13:12:26 +0000

    Specs for user groups

--- spec/models/user_group_spec.rb
+++ spec/models/user_group_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe UserGroup do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should respond_to :name }
+  it { should validate_presence_of :name }
+  it { should have_many :users }
 end
