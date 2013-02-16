be6842e4f6cdeb4754abf26b8b868333a8f2783b
Mark J Smith <mark@markjs.net>
2013-02-16 21:59:15 +0000

    Add default url host for development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,6 @@ OfficeDrinkRounds::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.delivery_method = :letter_opener
+
+  default_url_options[:host] = "office-drink-rounds.dev"
 end
