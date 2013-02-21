bb7bb6d93b2e9d5b0f89785bb07b7d32998b0c5c
Mark J Smith <mark@markjs.net>
2013-02-21 13:41:52 +0000

    Move find invite to filter method in invites controller

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,9 +1,9 @@
 class InvitesController < ApplicationController
 
   before_filter :find_user_group
+  before_filter :find_invite, except: [:new, :create]
 
   def show
-    @invite = @user_group.invites.find(params[:id])
     if request.path != user_group_invite_path(@user_group, @invite)
       redirect_to @user_group, alert: "Invite not found"
     end
@@ -29,4 +29,7 @@ private
     @user_group = UserGroup.find(params[:user_group_id])
   end
 
+  def find_invite
+    @invite = @user_group.invites.find(params[:id])
+  end
 end
